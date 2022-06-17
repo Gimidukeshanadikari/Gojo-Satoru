63025b142673a20342673d3114e9bb7048d1326c
Gimidukeshanadikari <106726007+Gimidukeshanadikari@users.noreply.github.com>
2022-06-17 21:35:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
+global.owner = ['0789108792']
 global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.ownernomer = '0789108792'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
